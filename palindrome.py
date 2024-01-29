1cb58916e519b163ec1c05f94d6ddb9e1d8183dc
Franzjodie Domingo <franzjodiedomingo@gmail.com>
2024-01-29 17:42:13 +0800

    test

--- palindrome.py
+++ palindrome.py
@@ -1,2 +1,9 @@
 # check if the number entered is a palindrome
 # using def function and if-else 
+
+def palindrome(num):
+    return str (num) == str(num) [::-1]
+
+number = 121
+if palindrome(number):
+    print(f"{number} is a  palindrome")
